c5741232578c101ee1ad6aae2da6d7559a010dff
Tom Tan <Tom.Tan@microsoft.com>
2023-01-04 11:19:08 -0800

    Update build script to build optimized tceetree

--- build.cmd
+++ build.cmd
@@ -1,1 +1,4 @@
-cl /Od /Zi /out:tceetree.exe tceetree.c gettree.c  outgraphviz.c  outtree.c  rbtree.c  slib.c  symtree.c
+cl /O2 /Zi /Fe:tceetree.exe tceetree.c gettree.c  outgraphviz.c  outtree.c  rbtree.c  slib.c  symtree.c /link /incremental:no
+
+@echo
+@echo Install the generated tceetree.exe to %PRITOOLS% ?
